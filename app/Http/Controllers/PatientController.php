35a04113235202fff23c761673ca9412c26cece2
crisvini <crisvini.leoncini@outlook.com>
2023-12-12 11:20:14 -0300

    criação dos controllers

---
+++ app/Http/Controllers/PatientController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Patient;
+use Illuminate\Http\Request;
+
+class PatientController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Patient $patient)
+    {
+    }
+
+    public function edit(Patient $patient)
+    {
+    }
+
+    public function update(Request $request, Patient $patient)
+    {
+    }
+
+    public function destroy(Patient $patient)
+    {
+    }
+}
---
+++ app/Http/Controllers/PatientTagController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PatientTag;
+use Illuminate\Http\Request;
+
+class PatientTagController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(PatientTag $patientTag)
+    {
+    }
+
+    public function edit(PatientTag $patientTag)
+    {
+    }
+
+    public function update(Request $request, PatientTag $patientTag)
+    {
+    }
+
+    public function destroy(PatientTag $patientTag)
+    {
+    }
+}
